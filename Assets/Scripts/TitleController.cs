679bdecc81b4d18b35a231a96336e1895e0f9886
SJH90 <wlghks1603@naver.com>
2018-08-05 19:04:53 +0900

    씬 기본 오브젝트 구성

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+	public GameObject PopupQuitGame;
+
+	public void ShowPopup(){
+		PopupQuitGame.SetActive(true);
+	}
+
+	public void HidePopup(){
+		PopupQuitGame.SetActive(false);
+	}
+
+	public void ChangeScene(int sceneNum){
+		SceneManager.LoadScene(sceneNum);
+	}
+
+	public void QuitApplication(){
+		Application.Quit();
+	}
+}
